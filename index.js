8e9bbe964677caf5f9996799d663c377bbe45b88
thmim <mdtaharimmim1010@gmail.com>
2025-06-06 05:32:35 +0600

    project setup

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@data-house.3s1f0x8.mongodb.net/?retryWrites=true&w=majority&appName=Data-house`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    
+  }
+}
+run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+    res.send('running hotet booking server site')
+})
+app.listen(port,()=>{
+    console.log(`hotet booking server is running on port: ${port}`)
+})
+
+
