937eb294d3fba0d271d09b202dec2c52b7631be8
thmim <mdtaharimmim1010@gmail.com>
2025-06-11 13:05:27 +0600

    added all hotels data

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const hotelsCollection = client.db('hotelBookings').collection('hotels');
+
+    // all hotels getting api
+    app.get('/hotels',async(req,res)=>{
+      const cursor = hotelsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
