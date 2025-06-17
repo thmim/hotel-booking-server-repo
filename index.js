78ea55567c1ec8d41f4e7d0d8ae14f585b0b5f3b
thmim <mdtaharimmim1010@gmail.com>
2025-06-17 06:14:54 +0600

    post hotels booking information

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@data-house.3s1f0x8.mongodb.net/?retryWrites=true&w=majority&appName=Data-house`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@data-house.3s1f0x8.mongodb.net/?retryWrites=true&w=majority&appName=Data-house`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,7 +25,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     
-    const hotelsCollection = client.db('hotelBookings').collection('hotels');
+    const hotelsCollection = client.db('hotesBookings').collection('hotels');
+    const guestsCollection = client.db('hotesBookings').collection('visitors');
 
     // all hotels getting api
     app.get('/hotels',async(req,res)=>{
@@ -32,6 +34,24 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    // my bookings data getting api
+    app.get('/visitors',async(req,res)=>{
+      const email = req.query.email
+      const query = {
+        guest:email
+      }
+      const result = await guestsCollection.find(query).toArray();
+      res.send(result);
+    })
+    // user data added api
+    app.post('/visitors',async(req,res)=>{
+      const guestInfo = req.body;
+      console.log(guestInfo)
+      const result = await guestsCollection.insertOne(guestInfo);
+      res.send(result);
+    })
+    
     // get data using id
     app.get('/hotels/:id',async(req,res)=>{
       const id = req.params.id;
